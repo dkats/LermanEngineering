926278d48cfcf2c934e331f7d20a84850434263e
Dan Kats <dkats12@gmail.com>
2022-04-13 15:24:00 -0400

    Switched order of work experience and Northeastern

--- js/header.js
+++ js/header.js
@@ -9,13 +9,6 @@ function header(level = 0) {
 	document.write('<ul class="menu">');
 	document.write('	<li class="noclick">Lerman Engineering</li>');
 	document.write('	<li class="dropdown noclick"');
-		document.write('		<span id="header_northeastern">Northeastern University</span>');
-		document.write('		<div class="droplist">');
-			document.write('		<span onclick="window.open(\'capstone.html\',\'_self\');" id="header_capstone">Capstone</span>');
-			document.write('		<span onclick="window.open(\'Baja-SAE.html\',\'_self\');" id="header_baja">Baja SAE</span>');
-		document.write('		</div>');
-		document.write('	</li>');
-	document.write('	<li class="dropdown noclick"');
 		document.write('		<span id="header_work">Work Experience</span>');
 		document.write('		<div class="droplist">');
 			document.write('		<span onclick="window.open(\'epam-continuum.html\',\'_self\');" id="header_epam">EPAM Continuum</span>');
@@ -25,6 +18,13 @@ function header(level = 0) {
 			document.write('		<span onclick="window.open(\'abiomed.html\',\'_self\');" id="header_abiomed">Abiomed</span>');
 			document.write('		<span onclick="window.open(\'the-simulation-group.html\',\'_self\');" id="header_sim_group">The Simulation Group</span>');
 			document.write('		<span onclick="window.open(\'carbone.html\',\'_self\');" id="header_carbone">Carbone Metal Fabricators</span>');
+		document.write('		</div>');
+		document.write('	</li>');
+	document.write('	<li class="dropdown noclick"');
+		document.write('		<span id="header_northeastern">Northeastern University</span>');
+		document.write('		<div class="droplist">');
+			document.write('		<span onclick="window.open(\'capstone.html\',\'_self\');" id="header_capstone">Capstone</span>');
+			document.write('		<span onclick="window.open(\'Baja-SAE.html\',\'_self\');" id="header_baja">Baja SAE</span>');
 		document.write('		</div>');
 		document.write('	</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'projects.html\',\'_self\')" id="header_proj">Other Projects</li>');
